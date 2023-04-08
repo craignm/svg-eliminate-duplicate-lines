40d5ad63c7c324fa5c575e290a5c2daac9438c6f
Craig Nevill-Manning <craig@sidewalklabs.com>
2023-04-07 23:44:50 -0400

    Collinear now works correctly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Read SVG into a list of path objects and list of dictionaries of attributes 
 from svgpathtools import svg2paths, wsvg, Path, Line
+from numpy import angle, pi
 paths, attributes = svg2paths('einstein hat2.svg')
 
 lines = {}
@@ -12,9 +13,9 @@ def add_line(p1, p2):
 
 def collinear_and_smaller(p1, p2, points):
     for p in points:
-        if (p != p1 and p != p2 and 
-            abs(p2 - p1) < abs(p - p1)):
-            if (p1.real - p2.real) * (p1.imag - p.imag) == (p1.real - p.real) * (p1.imag - p2.imag):
+        if p != p1 and p != p2 and abs(p2 - p1) < abs(p - p1):
+            if (abs(angle(p2 - p1) - angle(p - p1)) < 0.001 or
+                abs(angle(p2 - p1) - angle(p - p1)) > pi * 2 - 0.001):
                 return True
     return False
 
@@ -30,14 +31,14 @@ for start in lines:
         if (not (start, end) in unique_lines and
             not (end, start) in unique_lines and
             not collinear_and_smaller(start, end, lines[start]) and
-            not collinear_and_smaller(start, end, lines[end])):
+            not collinear_and_smaller(end, start, lines[end])):
             unique_lines[(start, end)] = True
 
 output_lines = []
 for line in unique_lines:
     output_lines.append(Line(line[0], line[1]))
 
-wsvg(output_lines, filename='output1.svg')
+wsvg(output_lines, filename='no_duplicate_lines.svg')
 
 
 #for line in unique_lines:
